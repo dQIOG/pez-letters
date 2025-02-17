aa719c2d19d071aadfa737fd09b26e5cb0838717
csae8092 <p.andorfer@gmail.com>
2025-02-17 13:40:40 +0100

    fix: add script to rename files

---
+++ rename_files.py
@@ -0,0 +1,12 @@
+import glob
+import os
+
+from acdh_tei_pyutils.tei import TeiReader
+
+for x in glob.glob("./data/editions/*.xml"):
+    doc = TeiReader(x)
+    path, _ = os.path.split(x)
+    xmlid = doc.any_xpath(".//@xml:id")[0]
+    new_name = f"{xmlid.replace}.xml"
+    new_path = os.path.join(path, new_name)
+    os.rename(x, new_path)
