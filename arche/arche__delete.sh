4ec65a31a98bb202aa1c77b9e43f196a75b7ff41
csae8092 <p.andorfer@gmail.com>
2025-02-17 18:47:14 +0100

    feat: try to improve delete script

--- arche/arche__delete.sh
+++ arche/arche__delete.sh
@@ -1,5 +1,27 @@
 #/bin/bash
 
+
+echo "deleting https://id.acdh.oeaw.ac.at/pez-briefe/vol-01-2010 from ${ARCHE}"
+docker run --rm \
+  --network="host" \
+  --entrypoint arche-delete-resource \
+  acdhch/arche-ingest \
+  "https://id.acdh.oeaw.ac.at/pez-briefe/vol-01-2010" ${ARCHE} ${ARCHE_USER} ${ARCHE_PASSWORD} --references
+  
+echo "deleting https://id.acdh.oeaw.ac.at/pez-briefe/vol-02-2015 from ${ARCHE}"
+docker run --rm \
+  --network="host" \
+  --entrypoint arche-delete-resource \
+  acdhch/arche-ingest \
+  "https://id.acdh.oeaw.ac.at/pez-briefe/vol-02-2015" ${ARCHE} ${ARCHE_USER} ${ARCHE_PASSWORD} --references
+  
+echo "deleting https://id.acdh.oeaw.ac.at/pez-briefe/pez-letters.rng from ${ARCHE}"
+docker run --rm \
+  --network="host" \
+  --entrypoint arche-delete-resource \
+  acdhch/arche-ingest \
+  "https://id.acdh.oeaw.ac.at/pez-briefe/pez-letters.rng" ${ARCHE} ${ARCHE_USER} ${ARCHE_PASSWORD} --references
+
 if [ "${TITLEIMAGE}" != "" ] ; then
   echo "deleting ${TITLEIMAGE} from ${ARCHE}"
   docker run --rm \
